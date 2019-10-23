03b5966b7afc72d45c3555b922996695f901f88b
airandiel <michaporebski@gmail.com>
2019-10-24 00:57:41 +0200

    iris classification

--- app/lib/naive_bayes.py
+++ app/lib/naive_bayes.py
@@ -1,64 +1,52 @@
 import numpy as np
+import random
 
 
-class NB(object):
-    # Split the dataset by class values, returns a dictionary
-    def separate_by_class(self, dataset):
-        separated = dict()
-        for i in range(len(dataset)):
-            vector = dataset[i]
-            class_value = vector[-1]
-            if (class_value not in separated):
-                separated[class_value] = list()
-            separated[class_value].append(vector)
-        return separated
+class NaiveBayes(object):
+    def calculate_summaries(self, dataset, labels):
+        """
+        Creates summary of dataset, first index - type of summary, second - no of class
 
+        :param dataset:
+        :return:
+        """
+        print(dataset, labels)
+        sorted_indices = np.argsort(labels)
+        dataset = dataset[sorted_indices]
+        labels = labels[sorted_indices]
+        dataset_classed = np.split(dataset, np.where(labels[:-1] != labels[1:])[0] + 1)
+        mean = np.array([np.mean(x, axis=0) for x in dataset_classed])
+        std = np.array([np.std(x, axis=0) for x in dataset_classed])
+        summary = [mean, std, [len(x) for x in dataset_classed]]
+        return summary
 
-    # Calculate the mean, stdev and count for each column in a dataset
-    def summarize_dataset(self, dataset):
-        summaries = [(np.mean(column), np.std(column), len(column)) for column in zip(*dataset)]
-        del (summaries[-1])
-        return summaries
-
-    # Split dataset by class then calculate statistics for each row
-    def summarize_by_class(self, dataset):
-        separated = self.separate_by_class(dataset)
-        summaries = dict()
-        for class_value, rows in separated.items():
-            summaries[class_value] = self.summarize_dataset(rows)
-        return summaries
+    def calculate_label_probabilities(self, rows, summary=None, mean=None, std=None):
+        if mean is None or std is None:
+            feature_probabilities = [self.calculate_probability(rows, summary[0][x], summary[1][x]) for x in
+                                     range(len(summary[0]))]
+        else:
+            feature_probabilities = [self.calculate_probability(rows, mean, std) for x in
+                                     range(len(summary[0]))]
+        probabilities = np.prod(feature_probabilities, axis=2) / summary[2][0]
+        return probabilities
 
     # Calculate the Gaussian probability distribution function for x
     def calculate_probability(self, x, mean, stdev):
-        exponent = np.exp(-((x-mean)**2 / (2 * stdev**2 )))
+        exponent = np.exp(-((x - mean) ** 2 / (2 * stdev ** 2)))
         return (1 / (np.sqrt(2 * np.pi) * stdev)) * exponent
 
-    # Calculate the probabilities of predicting each class for a given row
-    def calculate_class_probabilities(self, summaries, row):
-        total_rows = np.sum([summaries[label][0][2] for label in summaries])
-        probabilities = dict()
-        for class_value, class_summaries in summaries.items():
-            probabilities[class_value] = summaries[class_value][0][2]/np.float(total_rows)
-            for i in range(len(class_summaries)):
-                mean, stdev, count = class_summaries[i]
-                probabilities[class_value] *= self.calculate_probability(row[i], mean, stdev)
-        return probabilities
-
     # Predict the class for a given row
-    def predict(self, summaries, row):
-        probabilities = self.calculate_class_probabilities(summaries, row)
-        best_label, best_prob = None, -1
-        for class_value, probability in probabilities.items():
-            if best_label is None or probability > best_prob:
-                best_prob = probability
-                best_label = class_value
-        return best_label
+    def predict(self, summaries, rows):
+        probabilities = self.calculate_label_probabilities(rows, summaries)
+        best_labels = np.argmax(probabilities, axis=0)
+        return best_labels
 
     # Naive Bayes Algorithm
-    def naive_bayes(self, train, test):
-        summarize = self.summarize_by_class(train)
-        predictions = list()
-        for row in test:
-            output = self.predict(summarize, row)
-            predictions.append(output)
-        return (predictions)
+    def naive_bayes(self, train_data, train_labels, test_data):
+        summary = self.calculate_summaries(train_data, train_labels)
+        return self.predict(summary, test_data)
+        # predictions = list()
+        # for row in test:
+        #     output = self.predict(summarize, row)
+        #     predictions.append(output)
+        # return (predictions)
