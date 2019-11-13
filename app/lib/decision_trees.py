70be4714b25ed7a07e5b38f9023dcdf756e81022
cKaliban <cKaliban@users.noreply.github.com>
2019-11-13 13:56:14 +0100

    Decision tree structure rework

--- app/lib/decision_trees.py
+++ app/lib/decision_trees.py
@@ -1,13 +1,34 @@
 # Work in progress
+# Main decision tree class module
 import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.
 
 class DecisionTree():
-    def test(self):
+    def train(self):
+        #TODO: placeholder wrapper for tree creation based on separated part of dataset
         pass
 
-    def data(self):
+    def test(self):
+        #TODO: placeholder wrapper for tree testing based on separated part of dataset
         pass
-#
+
+    def split_data(self):
+        #TODO: wrapper for sklearn functions
+        pass
+
+    def create_tree(self, dataset=None, split=True, percentage=None):
+        #TODO: If no dataset provided, create a tree based on Iris dataset. If provided, split data or do not split, let user decide the percentage
+        if dataset is None:
+            dataset = load_iris()
+        
+        if split:
+            #Use 
+            pass
+        else:
+            #No default
+        pass
+
 if __name__ == "__main__":
     pass
 
--- app/plots/decision-trees/decision_trees.py
+++ app/plots/decision-trees/decision_trees.py
@@ -1,4 +1,5 @@
 # Work in progress
+# Module for plotting results based on the dataset provided
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,3 +9,7 @@ def execute(ax=None, **kwargs):
     ax = ax or plt.gca()
     line, = ax.plot (np.arange(0.0, 5.0, 0.02))
     return line
+
+def plot():
+    #TODO: plotting results based on default and chosen datasets
+    pass
