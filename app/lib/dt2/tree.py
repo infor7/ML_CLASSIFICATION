b87000aa9a6e2337966276f3d5206ecc86409fd6
cKaliban <cKaliban@users.noreply.github.com>
2019-12-06 04:26:21 +0100

    cleanup 1

--- app/lib/dt2/tree.py
+++ app/lib/dt2/tree.py
@@ -19,10 +19,9 @@ from sklearn.datasets import load_digits
 from sklearn.datasets import load_wine
 
 # Self implemented classes
-import tools
 from node import Node
 
-x = 0
+
 
 class Tree:
     def __init__(self, target, data, metric = "gini", max_depth = 0, min_split = 2, min_members = 1, min_gain = 0):
@@ -56,8 +55,6 @@ class Tree:
                     gain = new_gain
                     split_feature = feature
                     split_value = val
-        print(split_feature)            
-        print(split_value)
 
         if gain > self.min_gain and len(target) > self.min_split and (depth < self.max_depth or self.max_depth == 0):
             t_children = self.fit_cart(split_datasets[0], split_datasets[1], depth + 1)
@@ -75,7 +72,6 @@ class Tree:
         split_feature = None
         split_value = None
         split_datasets = None
-
         features_count = data.shape[1]
 
         for feature in range(0, features_count):
@@ -84,14 +80,47 @@ class Tree:
                 t_target, t_data, f_target, f_data = Node.split_data(target, data, feature, val)
                 p = (len(t_target) / (len(t_target) + len(f_target)))
                 new_gain = parent_score - p*Node.entropy(t_target) - (1-p)*Node.entropy(f_target)
+
                 if new_gain>gain and len(t_target)>=self.min_members and len(f_target)>=self.min_members:
-                    split_datasets = (t_target, t_data, f_target, f_data)
+                    t_data = Node.prune_feature(t_data, feature)
+                    f_data = Node.prune_feature(f_data, feature)
+                    split_datasets = [t_target, t_data, f_target, f_data]
                     gain = new_gain
                     split_feature = feature
                     split_value = val
-                    
-        
+
         if gain > self.min_gain and len(target) > self.min_split and (depth < self.max_depth or self.max_depth == 0) and features_count != 0:
+            t_children = self.fit_cart(split_datasets[0], split_datasets[1], depth + 1)
+            f_children = self.fit_cart(split_datasets[2], split_datasets[3], depth + 1)
+            return Node(target, data, False, depth, t_children, f_children, split_feature, split_value)
+        else:
+            return Node(target, data, True, depth)
+
+
+    def fit_id3_simple(self, target, data, depth):
+        """VERY Pseudo ID3 implementation
+         - no prunning, but can works with non-binary targets"""
+        parent_score = Node.entropy(target)
+        gain = 0.0
+        split_feature = None
+        split_value = None
+        split_datasets = None
+        features_count = data.shape[1]
+
+        for feature in range(0, features_count):
+            vals, counts = np.unique(data[:,feature], return_counts = True)
+            for val in vals:
+                t_target, t_data, f_target, f_data = Node.split_data(target, data, feature, val)
+                p = (len(t_target) / (len(t_target) + len(f_target)))
+                new_gain = parent_score - p*Node.entropy(t_target) - (1-p)*Node.entropy(f_target)
+
+                if new_gain>gain and len(t_target)>=self.min_members and len(f_target)>=self.min_members:
+                    split_datasets = [t_target, t_data, f_target, f_data]
+                    gain = new_gain
+                    split_feature = feature
+                    split_value = val
+
+        if gain > self.min_gain and len(target) > self.min_split and (depth < self.max_depth or self.max_depth == 0):
             t_children = self.fit_cart(split_datasets[0], split_datasets[1], depth + 1)
             f_children = self.fit_cart(split_datasets[2], split_datasets[3], depth + 1)
             return Node(target, data, False, depth, t_children, f_children, split_feature, split_value)
@@ -102,8 +131,10 @@ class Tree:
     def fit(self):
         if self.metric == "gini":
             return self.fit_cart(self.root.target, self.root.data, 0)
-        else:
+        elif self.metric == "entropy":
             return self.fit_id3(self.root.target, self.root.data, 0)
+        else:
+            return self.fit_id3_simple(self.root.target, self.root.data, 0)
 
     
     def classify(self, data, node):
@@ -116,7 +147,7 @@ class Tree:
                 else:
                     next_node = node.f_children
                 return self.classify(data, next_node)
-        else:
+        elif self.metric == "entropy":
             if node.leaf is True:
                 return node.result
             else:
@@ -124,51 +155,52 @@ class Tree:
                     next_node = node.t_children
                 else:
                     next_node = node.f_children
-                new_data = np.delete(data, feature, 1)
+                new_data = Node.prune_feature(data, node.feature)
                 return self.classify(new_data, next_node)
-
+        else:
+            if node.leaf is True: 
+                return node.result
+            else:
+                if data[node.feature] >= node.value:
+                    next_node = node.t_children
+                else:
+                    next_node = node.f_children
+                return self.classify(data, next_node)
+        
 
     @staticmethod
-    def cross_validate(folds, test_size, target, data, metric = "gini", max_depth = 0, min_split = 2, min_members = 1, min_gain = 0):
+    def cross_validate(folds, target, data, test_size = 0.25, metric = "gini", max_depth = 0, min_split = 2, min_members = 1, min_gain = 0.0):
         accuracy = 0.0
         precision = 0.0
         recall = 0.0
         for fold in range(0,folds):
-            data_train, data_test, target_train, target_test = train_test_split(target, data, test_size=test_size)
+            data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=test_size)
             tree = Tree(target_train, data_train, metric, max_depth, min_split, min_members, min_gain)
+            nodes = tree.fit()
+            predictions = []
+            
+            for row in data_test:
+                predictions.append(tree.classify(row, nodes))
+            accuracy += accuracy_score(target_test, predictions)
+            
+            if len(np.unique(target)) == 2:
+                precision += precision_score(target_test, predictions)
+                recall += recall_score(target_test, predictions)
 
+        accuracy = accuracy / folds
+        precision = precision / folds
+        recall = recall / folds
+        return accuracy, precision, recall
 
-        if len(np.unique(target)) == 2: return accuracy, precision, recall
-        else: return accuracy
-
-
-
-        
-
-
-    
 
 
 # Testcode
 def main():
+    """Cross-validation test for 3 sets and simple test for digits"""
     print("Running Decision Tree Example: iris")
     iris = load_iris()
-    iris_tree_g = Tree(iris.target, iris.data)
-    nodes = iris_tree_g.fit()
-
-
-
-
-    # print("Running Decision Tree Example: wine")
-    # wine = load_wine()
-
-
-    # print("Running Decision Tree Example: cancer")
-    # cancer = load_breast_cancer()
-
-    # # Warning - very slow
-    # print("Running Decision Tree Example: digits")
-
+    accuracy, prc, rc = Tree.cross_validate(10,iris.target, iris.data, 0.25, "simple")
+    print(accuracy)
 
 
 if __name__ == "__main__":
--- app/plots/decision-trees/decision_trees.py
+++ app/plots/decision-trees/decision_trees.py
@@ -1,22 +1,18 @@
-# Work in progress
-# Module for plotting results based on the dataset provided
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Mandatory execute function. I'm just writing those comments to guide my eyes 
 def execute(ax=None, **kwargs):
     ax = ax or plt.gca()
     line, = ax.plot (np.arange(0.0, 5.0, 0.02))
     return line
 
 def plot():
-    #TODO: plotting results based on default and chosen datasets
     pass
 
 
 if __name__ == "__main__":
-    # Dunno why yet. Just to be able to execute as main
+    
     fig, [plot1, plot2] = plt.subplots(nrows=2)
     execute(plot1)
     execute(plot2)
